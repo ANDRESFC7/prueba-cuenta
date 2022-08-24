65d11173430c9d498222398793334bccf6e14399
ANDRESFC7 <112009146+ANDRESFC7@users.noreply.github.com>
2022-08-24 12:17:26 -0500

    luz

--- main.ts
+++ main.ts
@@ -1,21 +1,13 @@
-input.onButtonPressed(Button.A, function () {
-    if (npersonas <= 0) {
-        npersonas += 0
-    } else {
-        npersonas += -1
-        basic.showString("" + (npersonas))
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    npersonas = 0
-    basic.showString("" + (npersonas))
-})
-input.onButtonPressed(Button.B, function () {
-    npersonas += 1
-    basic.showString("" + (npersonas))
-})
-let npersonas = 0
-npersonas = 0
 basic.forever(function () {
-	
+    if (input.lightLevel() <= 100) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # # # #
+            . # # # .
+            # . # . #
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
